3c353b60822d35246774ee5f1bab3410be0c230a
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-09-14 15:35:58 +0200

    add ecs delegation

--- game/scene/manager.go
+++ game/scene/manager.go
@@ -1,20 +1,38 @@
 package scene
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/yohamta/donburi/ecs"
+	"github.com/yohamta/donburi/features/events"
+)
+
+type Scenes map[string]*ecs.ECS
 
 type Manager struct {
-	current ebiten.Game
-	scenes  map[string]ebiten.Game
+	current *ecs.ECS
+	scenes  Scenes
 }
 
-func NewManager() *Manager {
-	m := &Manager{scenes: make(map[string]ebiten.Game)}
+func NewManager(initial string, scenes Scenes) *Manager {
+	if scenes == nil {
+		scenes = make(Scenes)
+	}
 
+	m := &Manager{scenes: scenes}
 	return m
 }
 
 func (m *Manager) Update() error {
+	if m.current != nil {
+		m.current.Update()
+		events.ProcessAllEvents(m.current.World)
+	}
+
 	return nil
 }
 
-func (m *Manager) Draw(screen *ebiten.Image) {}
+func (m *Manager) Draw(screen *ebiten.Image) {
+	if m.current != nil {
+		m.current.Draw(screen)
+	}
+}
